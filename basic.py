7c443470f985593a25da448e6db9cf2f1d7e5252
shruti1619 <136685394+shruti1619@users.noreply.github.com>
2025-12-30 18:58:49 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,13 @@
+age = 21
+print(age)
+print(type(age))
+
+
+name="shruti"
+
+print("my name is : "+name)
+print(type(name))
+
+is_completed= True #it can be false also
+print(is_completed)
+print(type(is_completed))
