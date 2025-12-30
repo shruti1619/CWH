692ea4069d2a9f3e1320951bac25a6d9a6e6f894
shruti1619 <136685394+shruti1619@users.noreply.github.com>
2025-12-30 19:01:56 +0530

    Add files via upload

---
+++ 02_scape_seqs.py
@@ -0,0 +1,16 @@
+
+
+'''Common escape sequences:
+\n: Newline
+\t: Tab
+\\: Backslash
+\": Double quote
+\': Single quote
+'''
+print("Hello\nWorld!")
+print("\nThis is a tab\tcharacter.")
+print("\nif i want to print blackslash then i have to add one more \\ \nlike this it going to print")
+#to print "\" (blakslash) we have to add one more like \\
+
+print("\nto add double quote add blackslash the that \" \t like this will print ")
+#to add double quote add blackslash the that \"  like this to print " same for single quote
---
+++ 03_print.py
@@ -0,0 +1,6 @@
+print("Hello", "World", sep=", ", end="!\n")
+
+print("shruti ", "kishor", 'morey')
+print("\nshruti", "kishor", 'morey', sep=',')#it will remove space by coma it made to replace coma
+
+print("\nshruti", "kishor", 'morey',end='............')#it will work as to add at end
