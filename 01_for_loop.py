0ce89a5fcc0bad525500b34b193bfb7757dbae4e
shruti1619 <136685394+shruti1619@users.noreply.github.com>
2025-12-30 19:11:58 +0530

    Add files via upload

---
+++ 01_for_loop.py
@@ -0,0 +1,15 @@
+
+# for i  in range (1,6):
+#     print(i)             #it will print from 1 to 5 because 6 is n-1 so 6-1=5
+
+
+
+# for i in range (5):
+#     print(i)  #it will print values from 0-4
+
+
+
+
+for i in range (1,11):
+    print("5 X",i ,'=' ,i)    #coma will automatically ad space and we can also write as print("5*",i,"=", 5*i) without i *=5c
+    i *=5         #or i can write i =i*5  table of 5 will print,
