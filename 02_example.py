bf5c15b8a1322b01bf80f2b29570d90c40a9cc24
shruti1619 <136685394+shruti1619@users.noreply.github.com>
2025-12-30 18:59:24 +0530

    Add files via upload

---
+++ 02_example.py
@@ -0,0 +1,30 @@
+str="10"
+print(str)
+print(type(str))
+
+integer=int(str)
+print(integer)
+print(type(integer))
+
+my="999"
+print(my)
+print(type(my))
+
+yours=float(my)
+print(yours)
+print(type(yours))
+
+# Convert string to integer
+num_str = "10"
+num_int = int(num_str)
+print(num_int)  # Output: 10
+
+# Convert integer to string
+num = 25
+num_str = float(num)
+print(num_str)  # Output: "25"
+
+# Convert float to integer
+pi = 3.14
+pi_int = int(pi)
+print(pi_int)   # Output: 3
